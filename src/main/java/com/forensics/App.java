e24d22d8628653aba37847d384118140d05da671
Nethra <nethraskk@gmail.com>
2026-09-05 22:44:03 +0530

    Initial commit: Base project setup

---
+++ src/main/java/com/forensics/App.java
@@ -0,0 +1,9 @@
+package com.forensics;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("==========================================");
+        System.out.println("  Digital Forensics Analyzer Initialized  ");
+        System.out.println("==========================================");
+    }
+}
